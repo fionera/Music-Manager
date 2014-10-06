b7858bc011762f5eb82f7432fed78a478caccfdd
fionera <fionera@fionera.de>
2014-10-06 16:25:42 +0200

    Create Commands.cs
    for bug search...

---
+++ Commands.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using ModAPI;
+
+namespace Commands
+{
+    public class Commands
+    {
+        ModAPI.Console.RegisterCommand("musicstop", musicstop);
+
+        public void musicstop(string params)
+            {
+                // Code
+            }
+    }
+}
